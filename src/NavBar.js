955f365879d5b066a430bfe0bb87a02e89e93da4
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-31 12:45:03 +0400

    changed logo

---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from "./KosaPlants_logo.png";
+
+const NavBar = () => {
+  return (
+    <div>
+        <img src = {Logo}/>
+    </div>
+  )
+}
+
+export default NavBar
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,18 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { clientProduct } from "./Client/products/Product";
-
+import NavBar from "./NavBar";
 
 export default function Root() {
-  clientProduct.listProducts()
-  .then(products => {
-      console.log('Products:', products);
-  })
-  .catch(error => {
-      console.error('Error fetching products:', error);
-  });
+  clientProduct
+    .listProducts()
+    .then((products) => {
+      console.log("Products:", products);
+    })
+    .catch((error) => {
+      console.error("Error fetching products:", error);
+    });
 
   return (
     <>
+      <NavBar />
       <Outlet />
     </>
   );
